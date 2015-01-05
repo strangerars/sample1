96ed7ba19f13dca532c14a9259028672665240f5
strangerars <strangerars@gmail.com>
2015-01-05 21:41:23 +0400

    initial

---
+++ Makefile
@@ -0,0 +1,4 @@
+do:
+	g++ sample1.cpp -o sample1
+clean:
+	rm sample1
---
+++ sample1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <limits>
+
+int main() 
+{
+  int i = std::numeric_limits<int>::max();
+  std::cout << i;
+
+}
